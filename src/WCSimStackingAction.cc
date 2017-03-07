84ca9afba9ebc2d2c12bc1916418e23129ecd4e1
Marcus O'Flaherty <moflaherty@sheffield.ac.uk>
2017-03-07 11:54:30 -0600

    fix double stacking action cut, bonsai mode

--- src/WCSimStackingAction.cc
+++ src/WCSimStackingAction.cc
@@ -82,45 +82,6 @@ G4ClassificationOfNewTrack WCSimStackingAction::ClassifyNewTrack
       }
     }*/
 
-  if( WCIDCollectionName2 =="ANNIEp2-glassFaceWCONLYLAPPDS"){
-  // Make sure it is an optical photon
-  if( particleType == G4OpticalPhoton::OpticalPhotonDefinition() )
-    {
-      //G4cout<<"entering ANNIEp2-glassFaceWCONLYLAPPDS in StackingAction....."<<G4endl; 
-      G4float photonWavelength = (2.0*M_PI*197.3)/(aTrack->GetTotalEnergy()/CLHEP::eV);
-      G4float ratio = 1./(1.0-0.25);
-      G4float wavelengthQE2 = 0;
-      if(aTrack->GetCreatorProcess()==NULL) {
-	wavelengthQE2  = DetConstruct->GetLAPPDQE(WCIDCollectionName2,photonWavelength,1,240,660,ratio);
-	if( G4UniformRand() > wavelengthQE2 ){
-	  classification = fKill;
-	  //G4cout<<"stage 0.."<<G4endl;
-	}
-      }
-      else if (((G4VProcess*)(aTrack->GetCreatorProcess()))->GetProcessType()!=3)
-	{
-	  G4float photonWavelength = (2.0*M_PI*197.3)/(aTrack->GetTotalEnergy()/CLHEP::eV);
-	  // MF : translated from skdetsim : better to increase the number of photons
-	  // than to throw in a global factor  at Digitization time !
-	  G4float ratio = 1./(1.0-0.25);
-	  // XQ: get the maximum QE and multiply it by the ratio
-	  // only work for the range between 240 nm and 660 nm for now 
-	  // Even with WLS
-	  G4float wavelengthQE2 = 0;
-	  if (DetConstruct->GetLAPPD_QE_Method()==1){ // G4cout<<"stage 1.."<<G4endl;
-	    wavelengthQE2  = DetConstruct->GetLAPPDQE(WCIDCollectionName2,photonWavelength,1,240,660,ratio);
-	  }else if (DetConstruct->GetLAPPD_QE_Method()==2){  //G4cout<<"stage 2.."<<G4endl;
-	    wavelengthQE2  = DetConstruct->GetLAPPDQE(WCIDCollectionName2,photonWavelength,0,240,660,ratio);
-	  }else if (DetConstruct->GetLAPPD_QE_Method()==3){ // G4cout<<"stage 3.."<<G4endl;
-	    wavelengthQE2 = 1.1;
-	  }
-	  
-	  if( G4UniformRand() > wavelengthQE2 )
-	    classification = fKill;
-	}
-    }
-  }//for lappds
-
   return classification;
 }
 
